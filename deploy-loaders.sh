f1a7a7dcc9d01b989b06f419b9fedbb5f7da5e9e
Alexander Kernozhitsky <sh200105@mail.ru>
2018-06-08 21:17:53 +0300

    Changes scp to rsync in deploy script

--- deploy-loaders.sh
+++ deploy-loaders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp -r cmscontrib/loaders judge@judge:/home/judge/cms/cms/cmscontrib/
+rsync -trl --del --progress cmscontrib/loaders/ judge@judge:/home/judge/cms/cms/cmscontrib/loaders/
 
 ssh -t judge@judge "./cmsrebuild.sh && ./cmsimporttest.sh"
